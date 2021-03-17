398fc3ae4a37183da8ea85f198cca6075a301656
Leonel Velez <leonel-14-velez@hotmail.com>
2021-03-17 09:16:07 -0500

    birrando el template search

--- www/js/app.js
+++ www/js/app.js
@@ -36,15 +36,6 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     controller: 'AppCtrl'
   })
 
-  .state('app.search', {
-    url: '/search',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/search.html'
-      }
-    }
-  })
-
   .state('app.browse', {
       url: '/browse',
       views: {
